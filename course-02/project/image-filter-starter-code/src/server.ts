5174973d764e544c2d0ac2fccd076742e858e0c1
sumanbgl <suman.bgl@gmail.com>
2022-07-02 01:25:56 +0530

    Implemented GET /filteredimage endpoint. (#2)
    * Added logic to filterimage and send processed file to client.
    
    * Fix the unit testing errors. Added eb dashboard screenshot.
    
    Co-authored-by: Suman Palavalli <suman.palavalli@boomi.com>

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -12,8 +12,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
@@ -28,12 +27,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+ 
+  app.get("/filteredimage", async( req: Request, res: Response ) => {
+    let { image_url } = req.query;   
 
-  //! END @TODO1
+    if (!image_url) {
+      return res.status(400).send(`image_url is required.`);      
+    }
+
+    let filtered_image_file = await filterImageFromURL(image_url);
+    res.sendFile(filtered_image_file);
+
+    let files_to_delete : Array<string> = new Array<string>();
+    files_to_delete.push(filtered_image_file);
+    res.on('finish', () => deleteLocalFiles(files_to_delete));
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
